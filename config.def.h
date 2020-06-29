3c2960318341af9a50ad045a1fef566fb3b00cc1
Mark Baierl <baierlmark@gmail.com>
2020-06-29 17:57:24 +0200

    xyw

--- config.def.h
+++ config.def.h
@@ -1,20 +1,31 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+static int dmx = 80; /* put dmenu at this x offset */
+static int dmy = 50; /* put dmenu at this y offset (measured from the bottom if topbar is 0) */
+static unsigned int dmw = 600; /* make dmenu this wide */
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
+
+static const char selBg[] = "#458588";
+static const char selFg[] = "#ebdbb2";
+static const char selHl[] = "#fabd2f";
+static const char bg[] = "#32302f";
+static const char fg[] = "#ebdbb2";//"#a89984";
+static const char hl[] = "#fabd2f";
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#a89984", "#282828" },
-	[SchemeSel] = { "#282828", "#458588" },
-	[SchemeSelHighlight] = { "#fabd2f", "#458588" },
-	[SchemeNormHighlight] = { "#ffc978","#282828" },
-	[SchemeOut] = { "#282828", "#8ec07c" },
+	[SchemeNorm] = { fg, bg },
+	[SchemeNormHighlight] = { hl, bg },
+	[SchemeSel] = { selFg, selBg },
+	[SchemeSelHighlight] = { selHl, selBg },
+	[SchemeOut] = { "#32302f", "#8ec07c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 10;
